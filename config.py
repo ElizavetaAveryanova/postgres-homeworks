fd7a2d004e0520a8b90f428945b7aab8229a47aa
ElizavetaAveryanova <153016425+ElizavetaAveryanova@users.noreply.github.com>
2024-02-23 23:39:12 +0500

    homework_1

---
+++ config.py
@@ -0,0 +1,10 @@
+import os.path
+
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+CUSTOMERS_DIR = os.path.join(ROOT_DIR, 'homework-1', 'north_data', 'customers_data.csv')
+EMPLOYEES_DIR = os.path.join(ROOT_DIR, 'homework-1', 'north_data', 'employees_data.csv')
+ORDERS_DIR = os.path.join(ROOT_DIR, 'homework-1', 'north_data', 'orders_data.csv')
+
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,39 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import os
+import psycopg2
+import csv
+from config import ORDERS_DIR, CUSTOMERS_DIR, EMPLOYEES_DIR
+
+# Подключаемся к базе данных
+conn = psycopg2.connect(host='localhost', database='north', user='postgres', password=os.getenv('PostgreSQL_PSW'))
+
+# Заполняем таблицы данными из north_data
+try:
+    with conn:
+        with conn.cursor() as cur:
+            with open(EMPLOYEES_DIR, encoding="windows-1251") as csvfile:
+                reader = csv.reader(csvfile)
+                next(reader) # пропускаем заголовки
+                # цикл для вставки данных в таблицу
+                for row in reader:
+                    query = 'INSERT INTO employees VALUES(%s,%s,%s,%s,%s,%s)'
+                    cur.execute(query, row)
+
+            with open(CUSTOMERS_DIR, encoding="windows-1251") as csvfile:
+                reader = csv.reader(csvfile)
+                next(reader) # пропускаем заголовки
+                # цикл для вставки данных в таблицу
+                for row in reader:
+                    query = 'INSERT INTO customers VALUES(%s,%s,%s)'
+                    cur.execute(query, row)
+
+            with open(ORDERS_DIR, encoding="windows-1251") as csvfile:
+                reader = csv.reader(csvfile)
+                next(reader) # пропускаем заголовки
+                # цикл для вставки данных в таблицу
+                for row in reader:
+                    query = 'INSERT INTO orders VALUES(%s,%s,%s,%s,%s)'
+                    cur.execute(query, row)
+
+finally:
+    conn.close()
